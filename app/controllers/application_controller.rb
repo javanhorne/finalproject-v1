1dd9c996a652f6e1d7c6be65a901a752d1aac21d
javanhorne <jvanhorn@uchicago.edu>
2020-05-07 04:51:15 +0000

    Finished product

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,57 @@
 class ApplicationController < ActionController::Base
 
+  def square_new
+    render({ :template => "square/new.html.erb"})
+  end  
+  
+  def square_results
+    @number = params.fetch("number")
+    @output = @number.to_d * @number.to_d
+    @output = @output.to_d
+    render({ :template => "square/results.html.erb"})
+  end 
+
+  def square_root_new
+    render({ :template => "square_root/new.html.erb"})
+  end  
+  
+  def square_root_results
+    @number = params.fetch("number")
+    @output = @number.to_d**(0.5)
+    
+    render({ :template => "square_root/results.html.erb"})
+  end  
+
+  def payment_new
+    render({ :template => "payment/new.html.erb"})
+  end  
+  
+  def payment_results
+    @apr = params.fetch("apr").to_d
+    @years = params.fetch("years").to_d
+    @prin = params.fetch("prin").to_d
+
+    n = @years* 12
+    r = (@apr / 12)/100
+    
+    @output = ((r * @prin)/(1-(1+r)**(-n)))
+    
+    render({ :template => "payment/results.html.erb"})
+  end  
+
+
+    def random_new
+    render({ :template => "random/new.html.erb"})
+  end  
+  
+  def random_results
+    @min = params.fetch("min").to_d
+    @max = params.fetch("max").to_d
+
+    
+    @output = rand(@min.to_f...@max.to_f)
+    
+    render({ :template => "random/results.html.erb"})
+  end  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,26 @@
 Rails.application.routes.draw do
 
+  get("/square/new", { :controller  => "application", :action => "square_new"})
+  get("/square/results", { :controller  => "application", :action => "square_results"})
+  get("/square_root/new", { :controller  => "application", :action => "square_root_new"})
+  get("/square_root/results", { :controller  => "application", :action => "square_root_results"})
+
+  get("/payment/new", { :controller  => "application", :action => "payment_new"})
+  get("/payment/results", { :controller  => "application", :action => "payment_results"})
+
+get("/random/new", { :controller  => "application", :action => "random_new"})
+  get("/random/results", { :controller  => "application", :action => "random_results"})
+
+
+
+
+
+
+
+
+
+
+
   # Write your routes above this line
   # =================================
 
